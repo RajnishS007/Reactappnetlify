cfcf835bdf958818600a9365f64fc3432e0a8c95
Rajnish kumar <mtalk007@gmail.com>
2023-01-06 21:42:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./store";
+import showResults from "./showResults";
+import SyncValidationForm from "./SyncValidationForm";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div style={{ padding: 15 }}>
+        <h2 style={{ textAlign: "center" }}>Sign up for Unacademy Plus!</h2>
+        <SyncValidationForm onSubmit={showResults} />
+      </div>
+    </Provider>
   );
-}
+};
 
 export default App;
